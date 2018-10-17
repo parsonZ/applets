69607978088873d86dc4fbc4271d7636bb3d4d52
pangzong <18001171283@163.com>
2018-10-17 16:10:48 +0800

    swiper

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,1 +1,19 @@
-Page({})
+Page({
+  data: {
+    swiperConfig: {
+      urls: [
+        '../../images/Desert.jpg',
+        '../../images/Koala.jpg',
+        '../../images/Tulips.jpg'
+      ],
+      indicatorDots: true,
+      indicatorColor: 'red',
+      indicatorActiveColor: 'blue',
+      autoPlay: true,
+      circular: true
+    }
+  },
+  scroll(){
+    console.log('adhskj')
+  }
+})
