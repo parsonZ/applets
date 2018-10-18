ca5ff9e7fb0742d996cfb1e62c985ea14e1a7ab8
pangzong <18001171283@163.com>
2018-10-18 11:24:31 +0800

    组件调试
    表单和导航组件

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -10,9 +10,32 @@ Page({
       indicatorActiveColor: 'blue',
       autoPlay: true,
       circular: true
+    },
+    movable: {
+      x: 10,
+      y: 20
+    },
+    icon: {
+      types: ['success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'],
+      size: 20,
+      color: 'green'
     }
   },
   scroll(){
     console.log('adhskj')
+  },
+  tap(){
+    this.setData({
+      movable: {
+        x: 40,
+        y: 40
+      }
+    })
+  },
+  getUser(detail){
+    console.log(detail)
+  },
+  formSubmit(e){
+    console.log(e.detail.value)
   }
 })
